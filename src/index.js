3949530763fe23b2ee37b7a254f6a3ff570b08e5
DBuckley0126 <DannyBoy.0126>
2020-01-17 11:45:40 +0000

    Initial branch from backend

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import _ from 'actioncable';
+
+document.addEventListener("DOMContentLoaded",() => {
+
+  let button = document.querySelector("#trigger-button")
+  let alert = document.querySelector("#alert")
+
+  window.App || (window.App = {})
+
+  App.cable = _.createConsumer("ws://javascript-project-gyro.herokuapp.com/cable")
+  App.message = App.cable.subscriptions.create("GameChannel", {
+    received: function(data){
+      alert.innerText = data["body"]["coor"]
+    },
+    sendMessage: function(messageBody){
+      this.perform('button_pressed', {body: messageBody})
+    }
+  })
+
+
+  window.addEventListener("mousemove", event => {
+    var x = event.clientX;     // Get the horizontal coordinate
+    var y = event.clientY;     // Get the vertical coordinate
+    var coor = "X coords: " + x + ", Y coords: " + y;
+
+    App.message.sendMessage({button_pressed: true, coor: coor})
+  })
+
+  
+
+  button.addEventListener("click", event => {
+    App.message.sendMessage({button_pressed: true})
+  })
+
+})
+
+
+
+
+
