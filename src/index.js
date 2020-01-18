dc80d7791f2fbdb2c090f1685f7542e5e0aee5b2
DBuckley0126 <DannyBoy.0126>
2020-01-18 00:52:36 +0000

    Change gyroscope sensor feedback to gamma

--- src/index.js
+++ src/index.js
@@ -99,14 +99,13 @@ document.addEventListener("DOMContentLoaded",() => {
 
     let x = 0
     window.addEventListener('deviceorientation', event => {
-      console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
       // x = event.beta;  // In degree in the range [-180,180]
-      x = event.beta; // In degree in the range [-90,90]
+      x = event.gamma; // In degree in the range [-90,90]
 
       // Because we don't want to have the device upside down
       // We constrain the x value to the range [-90,90]
-      // if (x >  90) { x =  90};
-      // if (x < -90) { x = -90};
+       if (x >  90) { x =  90};
+       if (x < -90) { x = -90};
 
       // To make computation easier we shift the range of
       // x and y to [0,180]
@@ -117,9 +116,10 @@ document.addEventListener("DOMContentLoaded",() => {
       // It center the positioning point to the center of the ball
       // ball.style.top  = (maxY*y/180 - 10) + "px";
       // ball.style.left = (maxX*x/180 - 10) + "px";
+      App.game.sendMessage({x: x})
     })
 
-    let loop = window.setInterval(()=>{App.game.sendMessage({x: x})}, 100)
+    // let loop = window.setInterval(()=>{App.game.sendMessage({x: x})}, 100)
   })
 
 })
